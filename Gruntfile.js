cdb647d8d2e3e7820ccd6e83be45a31dd46e0fbd
Shawn Melton <shawn.melton@techs-MacBook-Pro-35.local>
2017-05-12 09:31:48 -0400

    Attempting Sass integration to Heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = (function(grunt) {
                 dest: 'tmp/build.js'
             }
         },
-        sass: {
+        /*sass: {
             dist: {
                 files: {
                     'tmp/styles.css': 'web-src/css/styles.scss'
@@ -20,24 +20,13 @@ module.exports = (function(grunt) {
                     style: 'compressed'
                 }
             }
-        },
+        },*/
         copy: {
             prod: {
                 expand: true,
                 cwd: 'web-src',
                 src: ['index.html'],
                 dest: 'tmp'
-            }
-        },
-        uglify: {
-            options: {
-                mangle: false,
-                wrap: false
-            },
-            my_target: {
-                files: {
-                    'tmp/build.js': ['tmp/build.js']
-                }
             }
         },
         embed: {
@@ -53,25 +42,16 @@ module.exports = (function(grunt) {
         clean: {
             pre: ['html'],
             post: ['tmp']
-        },
-        es6transpiler: {
-            dist: {
-                files: {
-                    'tmp/build.js': 'tmp/build.js'
-                }
-            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    //grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-embed');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-es6-transpiler');
 
     grunt.registerTask('default', [
-        'clean:pre', 'copy', 'sass', 'browserify', /*'es6transpiler', 'uglify',*/ 'embed', 'clean:post'
+        'clean:pre', 'copy', /*'sass',*/ 'browserify', 'embed', 'clean:post'
     ]);
 });
