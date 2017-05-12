06964d80addd581e787ccc010d9bb6cc0eddfb11
Shawn Melton <shawn.melton@techs-MacBook-Pro-35.local>
2017-05-12 13:10:46 -0400

    Fix for heroku

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const app = express();
 twitterController.init(app);
 webController.init(app);
 
-app.listen(3000, function(req, res) {
+app.listen(process.env.PORT || 3000, function(req, res) {
     console.log('Service has successfully started.  All requests for this service should be routed through http://localhost:3000/.');
 });
